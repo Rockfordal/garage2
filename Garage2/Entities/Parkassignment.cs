21c541858af4998d14a75f90e400fc16fb6c1fc6
Anders Lundström <lundstrom123@gmail.com>
2016-11-17 10:44:18 +0100

    Parkasses

---
+++ Garage2/Entities/Parkassignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Entities
+{
+    public class Parkassignment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Fordon")]
+        public Vehicle Vehicle { get; set; }
+    }
+}
---
+++ Garage2/Repositories/SlotRepository.cs
@@ -0,0 +1,44 @@
+﻿using Garage2.DataAccess;
+using Garage2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Repositories
+{
+    public class SlotRepository
+    {
+        private GarageDb db = new GarageDb();
+
+        public SlotRepository()
+        { }
+
+        public bool Park(Vehicle v, Slot s)
+        {
+            if (s.Vehicle == null && v.Slot == null)
+            {
+                s.Vehicle = v;
+                v.Slot = s;
+                s.ParkTime = DateTime.Now;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Vehicle UnPark(Slot s)
+        {
+            if (s.Vehicle != null)
+            {
+                Vehicle v = s.Vehicle;
+                s.Vehicle = null;
+                v.Slot = null;
+                return s.Vehicle;
+            }
+            else
+                return null;
+        }
+
+    }
+}
